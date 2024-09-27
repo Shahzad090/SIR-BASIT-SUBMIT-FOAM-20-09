279903b49e56067379509f364d33bec95201d4d1
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-09-27 17:11:55 +0500

    done

--- app.js
+++ app.js
@@ -1,57 +1,32 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { getAuth, 
-         createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
-
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
+  import { getAuth, onAuthStateChanged,} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+  
+ 
   const firebaseConfig = {
-  apiKey: "AIzaSyANgtcbgJuF0sPEdlBhLi8yltx6nFcJaXw",
-  authDomain: "signup-form-using-firebase.firebaseapp.com",
-  projectId: "signup-form-using-firebase",
-  storageBucket: "signup-form-using-firebase.appspot.com",
-  messagingSenderId: "251898657374",
-  appId: "1:251898657374:web:f885c20ef29b54eb2792cf",
-  measurementId: "G-NQBTDTV4NH"
+    apiKey: "AIzaSyANgtcbgJuF0sPEdlBhLi8yltx6nFcJaXw",
+    authDomain: "fir-form-5e9ba.firebaseapp.com",
+    projectId: "fir-form-5e9ba",
+    storageBucket: "fir-form-5e9ba.appspot.com",
+    messagingSenderId: "171593386514",
+    appId: "1:171593386514:web:038733ffa3eb553885e6cb",
+    measurementId: "G-YHL2NXXTSW"
   };
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
   const auth = getAuth(app);
 
-  let name1 = document.getElementById('First name');
-
-//   let name = document.getElementById('Last name');
-
-let email = document.querySelector('#email');
-
-let password = document.querySelector('#password')
-
-let btn = document.querySelector('#btn');
 
 
-btn.addEventListener('click',  function () {
 
-  createUserWithEmailAndPassword(auth, email.value, password.value)
-
-      .then((userCredential) => {
-          // Signed up 
-          const user = userCredential.user;
-
-      Swal.fire("Form submitted successfully! Thank you.");
-
-          setTimeout(() => {
-              
-              window.location.href = 'login.html';
-
-          }, 500);
-
-      })
-
-      .catch((error) => {
-
-          const errorCode = error.code;
-          const errorMessage = error.message;
-
-          Swal.fire("Form submission failed. Please try again.");
-
-      });
-
-})
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user.uid;
+    
+  } else {
+    
+  }
+});
---
+++ pages/signUp.js
@@ -0,0 +1,54 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyANgtcbgJuF0sPEdlBhLi8yltx6nFcJaXw",
+    authDomain: "fir-form-5e9ba.firebaseapp.com",
+    projectId: "fir-form-5e9ba",
+    storageBucket: "fir-form-5e9ba.appspot.com",
+    messagingSenderId: "171593386514",
+    appId: "1:171593386514:web:038733ffa3eb553885e6cb",
+    measurementId: "G-YHL2NXXTSW"
+  };
+ 
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+ const analytics = getAnalytics(app);
+ const auth = getAuth(app);
+
+
+
+onAuthStateChanged(auth, (user) => {
+ if (user) {
+   const uid = user.uid;
+   
+ } else {
+   
+ }
+});
+
+
+  const signup_email = document.getElementById("signUp_email");
+  const signup_password = document.getElementById("signUp_password");
+  const signup_btn = document.getElementById("signup_btn");
+  signup_btn.addEventListener("click", createUserAccount);
+
+
+
+function createUserAccount() {
+    console.log("email=>", signup_email.value);
+    console.log("password=>", signup_password.value);
+    createUserWithEmailAndPassword(auth,signup_email.value,signup_password.value)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log("user=>", user)
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage)
+  });
+}
